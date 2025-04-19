9c9523d23cc4de68f7906d4d609f3e5c4f6531cd
pushpak8370 <pothuripushpak8370@gmail.com>
2025-04-19 20:45:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      target.scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+  
+ 
+  document.get
+  ::contentReference[oaicite:13]{index=13}
+   
+  
